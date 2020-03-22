cc7b9d233e3dd0c84a3a6505b354b217d8d1d414
Valentin Voigt <valentin.voigt@auf-nach-mallorca.info>
2020-03-22 20:30:41 +0100

    Statischen Pfad konfigurierbar machen

--- corona/routes.py
+++ corona/routes.py
@@ -1,14 +1,16 @@
 def includeme(config):
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    media_location = config.get_settings().get("media_location", "static")
+
+    config.add_static_view(path="static", cache_max_age=3600, name=media_location)
 
     # Statische Seiten
-    config.add_route('home', '/')
-    config.add_route('imprint', '/impressum')
+    config.add_route("home", "/")
+    config.add_route("imprint", "/impressum")
 
     # Login, Logout, Registrieren, ...
-    config.add_route('login', '/login')
-    config.add_route('logout', '/logout')
-    config.add_route('register', '/registrieren')
+    config.add_route("login", "/login")
+    config.add_route("logout", "/logout")
+    config.add_route("register", "/registrieren")
 
     # Dashboard
-    config.add_route('dashboard/organizations', '/login/organisationen')
+    config.add_route("dashboard/organizations", "/login/organisationen")
